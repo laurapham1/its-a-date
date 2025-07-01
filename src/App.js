e9ced2a732b04aa3d34ddc11fa94b03687778e72
Laura Pham <l.pham2311@gmail.com>
2025-07-01 20:31:43 +1000

    add select category

--- src/App.js
+++ src/App.js
@@ -1,34 +1,63 @@
-import { useState } from 'react';
-import dateIdeas from './dateIdeas.json';
-import './App.css';
+import { useState } from "react";
+import dateIdeas from "./dateIdeas.json";
+import "./App.css";
 
-const getRandomIdea = () => {
-  const randomIndex = Math.floor(Math.random() * dateIdeas.length);
-  return dateIdeas[randomIndex];
-}
+const dateCategories = [
+  "Outdoor",
+  "Creative",
+  "At Home",
+  "Adventurous",
+  "Food & Drink",
+  "Cosy & Chill",
+];
 
 function App() {
   const [activity, setActivity] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [category, setCategory] = useState("");
 
   const handleClick = () => {
     setLoading(true);
     setTimeout(() => {
       setActivity(getRandomIdea());
       setLoading(false);
-    }, 1000); 
+    }, 1000);
+  };
+
+  const getRandomIdea = () => {
+    const filteredIdeas = dateIdeas.filter((idea) =>
+      dateCategories.includes(category) ? idea.category === category : true
+    );
+    const randomIndex = Math.floor(Math.random() * filteredIdeas.length);
+    return filteredIdeas[randomIndex];
   };
 
   const content = () => {
     if (loading) return <p>Loading...</p>;
     if (!activity) return <p>No activity found.</p>;
 
-    return <h2>{activity.idea}</h2>;
+    return (
+      <>
+        <h2>{activity.idea}</h2>
+        <p>{activity.category}</p>
+      </>
+    );
   };
   return (
     <div className="App">
       <h1>It's a date! 🌹</h1>
-      <button onClick={handleClick} >Click me for an activity</button>
+      <label htmlFor="category">Choose a category:</label>
+      <select
+        className="select"
+        onChange={(e) => setCategory(e.target.value)}
+        value={category}
+      >
+        <option value="">Any</option>
+        {dateCategories.map((category) => (
+          <option value={category}>{category}</option>
+        ))}
+      </select>
+      <button onClick={handleClick}>Click me for an activity</button>
       <div className="content">{content()}</div>
     </div>
   );
